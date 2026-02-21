05391c125f7c03f5c5299065c12c62c4dfe0bd82
andersoncsquires <anderson.c.squires@gmail.com>
2026-02-21 17:00:38 -0500

    in a variable now

--- track3/filter.py
+++ track3/filter.py
@@ -4,11 +4,13 @@ column_index = 9
 with open("Crime_Data/Crime_Data_2023-1.csv", "r") as f:
     test = f.readline()
     reader = csv.reader(f)
+    list12023 = []
     for row in reader:
         #print(row)
         lat = row[9]
         long = row[10]
-        print(lat, long)
+        list12023.append(lat + " " + long)
+print(list12023)
 
 ### csv parse -> row
 ### row -> x,y for the 2D list (from long and lat), type of crime, and
