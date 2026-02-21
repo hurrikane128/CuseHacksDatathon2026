e62710d643f6d3192ea2a631e58bc18191487f7c
andersoncsquires <anderson.c.squires@gmail.com>
2026-02-21 18:08:56 -0500

    filter for loop (commented out)

--- track3/Test.py
+++ track3/Test.py
@@ -1,3 +1,21 @@
-import plotly.express as px
-fig = px.bar(x=["a", "b", "c"], y=[1, 3, 2])
+import plotly.graph_objects as go
+import numpy as np
+#insert array here
+Array = [
+    []
+]
+z_data = np.array(Array)
+
+fig = go.figure(data=go.Heatmap(
+    z=z_data,
+    colorscale="Plasma",
+    colorbar=dict(title="Density")
+))
+fig.update_layout(
+    title="Density of crime",
+    xaxis_title="",
+    yaxis_title="",
+    width=600,
+    height=600
+)
 fig.show()
