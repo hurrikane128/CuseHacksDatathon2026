dcdbde8fd95218406495fc6335cea4c62304b76e
andersoncsquires <anderson.c.squires@gmail.com>
2026-02-21 16:12:19 -0500

    lat for 2023

--- track3/filter.py
+++ track3/filter.py
@@ -1,3 +1,7 @@
-with open("Crime_Data_2022-1.csv", "r") as f:
-    data = f.read()
-    print(data)
+#lat for 2023
+import csv
+column_index = 10
+with open("Crime_Data_2023-1.csv", "r") as f:
+    reader = csv.reader(f)
+    column = [row[column_index] for row in reader]
+print(column)
