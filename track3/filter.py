d9363ef8a5d1aaff8f3ba708a79150a3299cc880
andersoncsquires <anderson.c.squires@gmail.com>
2026-02-21 16:45:24 -0500

    fix the lat

--- track3/filter.py
+++ track3/filter.py
@@ -1,6 +1,6 @@
 #lat for 2023
 import csv
-column_index = 10
+column_index = 9
 with open("Crime_Data/Crime_Data_2023-1.csv", "r") as f:
     reader = csv.reader(f)
     column = [row[column_index] for row in reader]
