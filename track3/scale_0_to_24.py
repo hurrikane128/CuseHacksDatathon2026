377b7fccc5bff2fdfc3cb418780cdf7ee818b7fd
matthewtorres <matthew.torres.fsjs07@gmail.com>
2026-02-21 18:06:19 -0500

    scale indexed numbers from 0 to 24

---
+++ track3/scale_0_to_24.py
@@ -0,0 +1,12 @@
+def latlon_to_grid(latitude, longitude, min_lat, max_lat, min_lon, max_lon, grid_size=25):
+
+    lat_norm = (latitude - min_lat) / (max_lat - min_lat)
+    lon_norm = (longitude - min_lon) / (max_lon - min_lon)
+
+    lat_index = int(lat_norm * (grid_size - 1))
+    lon_index = int(lon_norm * (grid_size - 1))
+
+    lat_index = max(0, min(lat_index, grid_size - 1))
+    lon_index = max(0, min(lon_index, grid_size - 1))
+
+    return lat_index, lon_index
