126b8e6c09cce60651825adebeecd10076b37a79
jhonnik08 <137032559+jhonnik08@users.noreply.github.com>
2026-02-21 16:42:10 -0500

    Comment with the plan added

--- track3/filter.py
+++ track3/filter.py
@@ -1,8 +1,18 @@
 #lat for 2023
 import csv
-column_index_lat = 10
+column_index = 10
 with open("Crime_Data/Crime_Data_2023-1.csv", "r") as f:
     reader = csv.reader(f)
-    column = [row[column_index_lat] for row in reader]
-latitudes20231 = column
+    column = [row[column_index] for row in reader]
 print(column)
+
+### csv parse -> row
+### row -> x,y for the 2D list (from long and lat), type of crime, and
+
+# [
+#   [{}, {}, {}],
+#   [{}, {}, {}],
+#   [{}, {}, {}],
+# ]
+
+# { "LARCENY":1, ... }
