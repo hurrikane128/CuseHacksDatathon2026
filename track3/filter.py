d1a7f3eb625e639e986dc70d35f76491a01402c9
andersoncsquires <anderson.c.squires@gmail.com>
2026-02-21 17:09:22 -0500

    added all

--- track3/filter.py
+++ track3/filter.py
@@ -1,6 +1,5 @@
 #lat for 2023
 import csv
-column_index = 9
 with open("Crime_Data/Crime_Data_2023-1.csv", "r") as f:
     test = f.readline()
     reader = csv.reader(f)
@@ -11,7 +10,46 @@ with open("Crime_Data/Crime_Data_2023-1.csv", "r") as f:
         long = row[10]
         list12023.append(lat)
         list12023.append(long)
+
+with open("Crime_Data/Crime_Data_2023-2.csv", "r") as f:
+    test = f.readline()
+    reader = csv.reader(f)
+    list22023 = []
+    for row in reader:
+        #print(row)
+        lat = row[9]
+        long = row[10]
+        list22023.append(lat)
+        list22023.append(long)
+
+with open("Crime_Data/Crime_Data_2024-1.csv", "r") as f:
+    test = f.readline()
+    reader = csv.reader(f)
+    list12024 = []
+    for row in reader:
+        #print(row)
+        lat = row[9]
+        long = row[10]
+        list12024.append(lat)
+        list12024.append(long)
+
+with open("Crime_Data/Crime_Data_2024-2.csv", "r") as f:
+    test = f.readline()
+    reader = csv.reader(f)
+    list22024 = []
+    for row in reader:
+        #print(row)
+        lat = row[9]
+        long = row[10]
+        list22024.append(lat)
+        list22024.append(long)
 print(list12023)
+print("\n")
+print(list22023)
+print("\n")
+print(list12024)
+print("\n")
+print(list22024)
 
 ### csv parse -> row
 ### row -> x,y for the 2D list (from long and lat), type of crime, and
