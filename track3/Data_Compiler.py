3bf64e030ebed6e98a795e9b218d2153a40725ee
matthewtorres <matthew.torres.fsjs07@gmail.com>
2026-02-21 16:12:51 -0500

    Refining datafiles

--- track3/Data_Compiler.py
+++ track3/Data_Compiler.py
@@ -1,3 +1,10 @@
 import pandas as pd
 
-data_files = []
+data_files = [
+    "Crime_Data_2022-1.csv",
+    "Crime_Data_2022-2.csv",
+    "Crime_Data_2023-1.csv",
+    "Crime_Data_2023-2.csv",
+    "Crime_Data_2024-1.csv",
+    "Crime_Data_2024-2.csv",
+]
