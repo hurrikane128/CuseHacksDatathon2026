d8162976ab6d92452917feef04d7a3b906f54c3d
bobby <bobbyrenny12@gmail.com>
2026-02-21 16:52:29 -0500

    Created a list of dictionaries containing the cords and crime

--- track3/Crime_Data/Data_Compiler.py
+++ track3/Crime_Data/Data_Compiler.py
@@ -2,14 +2,16 @@ import csv
 
 import pandas as pd
 
-data_files = ["Crime_Data_2024-1.csv"]
+data_files = ["Crime_Data_2023-1.csv", "Crime_Data_2023-2.csv", "Crime_Data_2024-1.csv", "Crime_Data_2024-2.csv"]
 
-crime_dict = {}
+crime_dict = []
 
 for item in data_files:
     with open(item) as cur_file:
         info = csv.reader(cur_file)
         for row in info:
-            print("X: " + str(row[0]), "Y: " + str(row[1]), "Crime: " + str(row[8]))
+            crime_dict.append({(row[0], row[1]): row[8]})
 
+for index, item in enumerate(crime_dict):
+    print(crime_dict[index])
 
