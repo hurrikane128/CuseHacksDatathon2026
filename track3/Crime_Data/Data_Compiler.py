5a24375dad38bdb9204b0a30da3cedb34d975fdc
bobby <bobbyrenny12@gmail.com>
2026-02-21 16:41:13 -0500

    Crime and coords initial

--- track3/Crime_Data/Data_Compiler.py
+++ track3/Crime_Data/Data_Compiler.py
@@ -1,13 +1,15 @@
+import csv
+
 import pandas as pd
 
-data_files = ["Crime_Data_2024-1.csv", "Crime_Data_2024-2.csv"]
-
+data_files = ["Crime_Data_2024-1.csv"]
 
 crime_dict = {}
 
 for item in data_files:
-    with open(item, "r") as cur_file:
-        info = cur_file.readlines()
-        print(info)
+    with open(item) as cur_file:
+        info = csv.reader(cur_file)
+        for row in info:
+            print("X: " + str(row[0]), "Y: " + str(row[1]), "Crime: " + str(row[8]))
 
 
