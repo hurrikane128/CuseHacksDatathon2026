19ff5e065070fa6a2113001530fa81bcca14f729
matthewtorres <matthew.torres.fsjs07@gmail.com>
2026-02-22 11:05:52 -0500

    ADDING COMMENTS NOTHING MORE STG

--- track3/ScaleCoordsToIndex.py
+++ track3/ScaleCoordsToIndex.py
@@ -1,7 +1,15 @@
 def latlon_to_grid(latitude, longitude, min_lat, max_lat, min_lon, max_lon, grid_size=25):
 
+######################
+# INCOMING LAT/LONG CONVERSION
+######################
+
     lat_norm = (latitude - min_lat) / (max_lat - min_lat)
     lon_norm = (longitude - min_lon) / (max_lon - min_lon)
+
+######################
+# INDEXING CORRECT SIZE
+######################
 
     lat_index = int(lat_norm * (grid_size - 1))
     lon_index = int(lon_norm * (grid_size - 1))
